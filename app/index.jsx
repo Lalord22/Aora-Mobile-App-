1d0773aa6609714c4824f6c8ad8b26fbefde87f3
Lalord22 <lalo22296@hotmail.com>
2024-08-29 17:37:15 -0600

    Adds profile link

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
 export default function App(){
   return (
     <View style={styles.container}>
         <Text>Aora!</Text>
         <StatusBar style="auto" />
+        <Link href="/profile" style={{color:'blue'}}>
+        Profile
+        </Link>
     </View>
   )
 }
