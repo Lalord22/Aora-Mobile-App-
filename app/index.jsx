5a3b98424d2f3c1d23ce363a719d7466532c74c0
Lalord22 <lalo22296@hotmail.com>
2024-08-30 17:39:48 -0600

    Adds login form

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Image, ScrollView, Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Link } from 'expo-router'
+import { Link, Redirect, router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../constants'
 import CustomButton from '../components/CustomButton'
@@ -9,7 +9,7 @@ export default function App(){
   return (
     <SafeAreaView className="bg-primary h-full">
         <ScrollView contentContainerStyle={{height: '100%'}}>
-            <View className='w-full justify-center items-center h-full px-4'>
+            <View className='w-full justify-center items-center min-h-[85vh] px-4'>
                 <Image  
                     source={images.logo}
                     className= " w-[130px] h-[84px]"
@@ -39,9 +39,14 @@ export default function App(){
                     Made with ❤️ by  Lalo {"\n"}
                     Inspired by JavaScript Mastery Crash Course
                 </Text>
-                <CustomButton />
+                <CustomButton 
+                    title="Continue with Email"
+                    handlePress={()=> router.push('/sign-in')}
+                    containerStyles="w-full mt-7"
+                />
             </View>
         </ScrollView>
+        <StatusBar backgroundColor='#161622' style="light" />
     </SafeAreaView>
   )
 }
